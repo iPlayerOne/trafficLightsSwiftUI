b6d57b4df542b200e10aa4a6193a66d842b60fbe
Ivan Korobov <ivan.koroboff@gmail.com>
2022-09-08 14:17:45 +0300

    Initial Commit

---
+++ trafficLightsSwiftUI/trafficLightsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  trafficLightsSwiftUIApp.swift
+//  trafficLightsSwiftUI
+//
+//  Created by ikorobov on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct trafficLightsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
